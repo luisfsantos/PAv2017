2d111a03e4788d38c2b5e9fcb7e77a2794de9240
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2017-03-22 16:16:08 +0000

    add javassist lib and translator

---
+++ JavaOptionalArguments/src/ist/meic/pa/KeyWordArgsTranslator.java
@@ -0,0 +1,21 @@
+package ist.meic.pa;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.NotFoundException;
+import javassist.Translator;
+/**
+ * Created by lads on 22/03/2017.
+ */
+public class KeyWordArgsTranslator implements Translator {
+
+    @Override
+    public void start(ClassPool classPool) throws NotFoundException, CannotCompileException {
+
+    }
+
+    @Override
+    public void onLoad(ClassPool classPool, String s) throws NotFoundException, CannotCompileException {
+
+    }
+}
