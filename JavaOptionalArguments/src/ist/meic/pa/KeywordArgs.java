96e39e6cd2c92f8104820581297c83ca6403c414
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-03-23 06:46:29 +0000

    feat: command-line provided class is now loaded
    The class provided as a command line arg is now being loaded. This has been achieved by fixing the order of opretations, as well as addint the current dir to the classpath.
    
    additions to #1

--- JavaOptionalArguments/src/ist/meic/pa/KeywordArgs.java
+++ JavaOptionalArguments/src/ist/meic/pa/KeywordArgs.java
@@ -1,8 +1,15 @@
 package ist.meic.pa;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by lads on 22/03/2017.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.CONSTRUCTOR)
 public @interface KeywordArgs {
     String value() default "";
 }
