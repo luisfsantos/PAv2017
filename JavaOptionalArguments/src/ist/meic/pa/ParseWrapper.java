91a09f475ec0fa7bd649bb284e779469d00cc8ae
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2017-03-24 18:37:17 +0000

    passes test A and H was just to see how project works more or less

--- JavaOptionalArguments/src/ist/meic/pa/ParseWrapper.java
+++ JavaOptionalArguments/src/ist/meic/pa/ParseWrapper.java
@@ -1,8 +1,12 @@
 package ist.meic.pa;
 
 import javassist.CtClass;
+import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
+import org.jgrapht.graph.DefaultEdge;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by lads on 23/03/2017.
@@ -12,6 +16,7 @@ public class ParseWrapper {
     KeywordArgs kwAnnotation;
     CtClass ctClass;
     HashMap<String, ValueWrapper> values = new HashMap<>();
+    DirectedAcyclicGraph<String, DefaultEdge> dependancies = new DirectedAcyclicGraph<>(DefaultEdge.class);
 
     public ParseWrapper(KeywordArgs keywordArgs, CtClass ctClass) {
         kwAnnotation = keywordArgs;
@@ -19,8 +24,19 @@ public class ParseWrapper {
     }
 
     public HashMap<String, ValueWrapper> parse() {
+        String toParse = kwAnnotation.value();
+        String[] arguments = toParse.split(",");
+        for (String kwWord : arguments) {
+            kwWord.trim();
+            String[] leftRight = kwWord.split("=");
+            values.putIfAbsent(leftRight[0], new ValueWrapper(leftRight[1]));
+        }
         //TODO: regex code which also puts in the values all the inherited values from the super classes
         return values;
+    }
+
+    public List<String> getSortedParameters() {
+        return new LinkedList<>();
     }
 
 
