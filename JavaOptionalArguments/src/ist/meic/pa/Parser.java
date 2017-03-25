77252afb3c8e5faf8864aa2a169ad4caaa15b489
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-03-25 22:10:31 +0000

    feat: strings with commas in value are now parsed correctly
    All related tests pass. Addtions to #3.

--- JavaOptionalArguments/src/ist/meic/pa/Parser.java
+++ JavaOptionalArguments/src/ist/meic/pa/Parser.java
@@ -3,12 +3,14 @@ package ist.meic.pa;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.logging.Logger;
 
 /**
  * Class responsible for parsing an expression into a Key-Value HashMap.
  */
 public class Parser {
-    private final static String PATTERN = "(?<varName>.+?)(=(?<varValue>.+?)(,|$)|(,|$))";
+    private final static String PATTERN = "(?<varName>.+?)(=(?<varValue>(\".+?\"|.+?))(,|$)|(,|$))";
+
 
     public static HashMap<String, String> parse(String expr) {
         Pattern p = Pattern.compile(PATTERN);
@@ -20,7 +22,7 @@ public class Parser {
         while(m.find()) {
             varName = m.group("varName");
             varValue = m.group("varValue");
-            System.out.println(varName + ":" + varValue);
+            // System.out.println(varName + ":" + varValue);
             result.put(varName, varValue);
         }
 
